165a4d9525de2bead7cd4c34186ade84c1c3100f
Nan <85908109+HannahWroblewski@users.noreply.github.com>
2024-05-03 17:20:13 -0400

    moved createUser to it's own component

--- cs-demo/client/src/App.tsx
+++ cs-demo/client/src/App.tsx
@@ -2,16 +2,9 @@ import React, { useState } from 'react';
 import { ApolloProvider, ApolloClient, InMemoryCache, useMutation } from '@apollo/client';
 import { CREATE_USER } from "./graphql/Mutation";
 import './App.css';
+import CreateUser from './components/CreateUser';
 
 function App() {
-
-  const [name, setName] = useState("");
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [yearsInPractice, setYearsInPractice] = useState(0);
-  const [influence, setInfluence] = useState("");
-
-  const [createUser, { error }] = useMutation(CREATE_USER);
 
   const client = new ApolloClient({
     uri: 'http://localhost:3001/graphql',
@@ -20,61 +13,7 @@ function App() {
 
   return (
     <ApolloProvider client={client}>
-      <div className='createUser'>
-        <input
-          type="text"
-          placeholder='Name'
-          onChange={(e) => {
-            setName(e.target.value);
-            console.log("NAME: ", name); 
-          }}
-        />
-        <input
-          type="text"
-          placeholder='Username'
-          onChange={(e) => {
-            setUsername(e.target.value);
-            console.log("USERNAME: ", username); 
-          }}
-        />
-        <input
-          type="password"
-          placeholder='Password'
-          onChange={(e) => {
-            setPassword(e.target.value);
-            console.log("PASSWORD: ", password); 
-          }}
-        />
-        <input
-          type="number"
-          placeholder='Years in Practice'
-          onChange={(e) => {
-            setYearsInPractice(Number(e.target.value));  
-            console.log("YEARS IN PRACTICE: ", yearsInPractice);  
-          }}
-        />
-        <input
-          type="text"
-          placeholder='Influence'
-          onChange={(e) => {
-            setInfluence(e.target.value);
-            console.log("INFLUENCE: ", influence);  
-          }}
-        />
-        <button onClick={() => {
-          createUser(
-            {
-              variables: {
-                name: name,
-                username: username,
-                password: password,
-                yearsInPractice: yearsInPractice,
-                influence: influence
-            }
-            }
-          )
-        }}>Create User</button>
-      </div>
+      <CreateUser />
     </ApolloProvider>
   );
 }
--- cs-demo/client/src/components/CreateUser.tsx
+++ cs-demo/client/src/components/CreateUser.tsx
@@ -1,5 +1,71 @@
-import React from "react";
+import { useMutation } from "@apollo/client";
+import React, { useState } from "react";
+import { CREATE_USER } from "../graphql/Mutation";
 
-// export const CreateUser() {
+export default function CreateUser() {
+    const [name, setName] = useState("");
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [yearsInPractice, setYearsInPractice] = useState(0);
+    const [influence, setInfluence] = useState("");
+  
+    const [createUser, { error }] = useMutation(CREATE_USER);
 
-// }
+    return (
+        <div className='createUser'>
+        <input
+          type="text"
+          placeholder='Name'
+          onChange={(e) => {
+            setName(e.target.value);
+            console.log("NAME: ", name); 
+          }}
+        />
+        <input
+          type="text"
+          placeholder='Username'
+          onChange={(e) => {
+            setUsername(e.target.value);
+            console.log("USERNAME: ", username); 
+          }}
+        />
+        <input
+          type="password"
+          placeholder='Password'
+          onChange={(e) => {
+            setPassword(e.target.value);
+            console.log("PASSWORD: ", password); 
+          }}
+        />
+        <input
+          type="number"
+          placeholder='Years in Practice'
+          onChange={(e) => {
+            setYearsInPractice(Number(e.target.value));  
+            console.log("YEARS IN PRACTICE: ", yearsInPractice);  
+          }}
+        />
+        <input
+          type="text"
+          placeholder='Influence'
+          onChange={(e) => {
+            setInfluence(e.target.value);
+            console.log("INFLUENCE: ", influence);  
+          }}
+        />
+        <button onClick={() => {
+          createUser(
+            {
+              variables: {
+                name: name,
+                username: username,
+                password: password,
+                yearsInPractice: yearsInPractice,
+                influence: influence
+            }
+            }
+          )
+        }}>Create User</button>
+      </div>
+    );
+}
