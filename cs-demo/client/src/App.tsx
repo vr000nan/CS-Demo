162a2653869b04927eacacc7d6c97fd33be7ce38
Nan <85908109+HannahWroblewski@users.noreply.github.com>
2024-05-03 16:16:52 -0400

    added useStates and onChange to fields

--- cs-demo/client/src/App.tsx
+++ cs-demo/client/src/App.tsx
@@ -1,9 +1,13 @@
 import React, { useState } from 'react';
 import { ApolloProvider, ApolloClient, InMemoryCache, useMutation } from '@apollo/client';
-import { CREATE_USER } from './graphql/Mutation';
 import './App.css';
 
 function App() {
+  const [name, setName] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [yearsInPractice, setYearsInPractice] = useState(0);
+  const [influence, setInfluence] = useState("");
 
   const client = new ApolloClient({
     uri: 'http://localhost:3001/graphql',
@@ -16,22 +20,42 @@ function App() {
         <input
           type="text"
           placeholder='Name'
+          onChange={(e) => {
+            setName(e.target.value);
+            console.log("NAME: ", name); 
+          }}
         />
         <input
           type="text"
           placeholder='Username'
+          onChange={(e) => {
+            setUsername(e.target.value);
+            console.log("USERNAME: ", username); 
+          }}
         />
         <input
           type="password"
           placeholder='Password'
+          onChange={(e) => {
+            setPassword(e.target.value);
+            console.log("PASSWORD: ", password); 
+          }}
         />
         <input
           type="number"
           placeholder='Years in Practice'
+          onChange={(e) => {
+            setYearsInPractice(Number(e.target.value));  
+            console.log("YEARS IN PRACTICE: ", yearsInPractice);  
+          }}
         />
         <input
           type="text"
           placeholder='Influence'
+          onChange={(e) => {
+            setInfluence(e.target.value);
+            console.log("INFLUENCE: ", influence);  
+          }}
         />
         <button >Create User</button>
       </div>
